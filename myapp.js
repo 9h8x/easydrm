0dbf8f28438390b62e62e7fa9097994e91bd434c
Juan <76052920+9h8x@users.noreply.github.com>
2024-07-30 22:28:33 -0300

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,62 @@
+const manifestUri = 'https://edge-live02-mun.cvattv.com.ar/live/c7eds/AnimalPlanet/SA_Live_dash_enc/AnimalPlanet.mpd';
+
+function initApp() {
+  // Install built-in polyfills to patch browser incompatibilities.
+  shaka.polyfill.installAll();
+
+  // Check to see if the browser supports the basic APIs Shaka needs.
+  if (shaka.Player.isBrowserSupported()) {
+    // Everything looks good!
+    initPlayer();
+  } else {
+    // This browser does not have the minimum set of APIs we need.
+    console.error('Browser not supported!');
+  }
+}
+
+async function initPlayer() {
+  // Create a Player instance.
+  const video = document.getElementById('video');
+  const player = new shaka.Player(video);
+  
+  // Attach player to the window to make it easy to access in the JS console.
+  window.player = player;
+
+  // Listen for error events.
+  player.addEventListener('error', onErrorEvent);
+
+  // Configure the DRM settings.
+  player.configure({
+    drm: {
+      servers: {
+        'org.w3.clearkey': '' // No server URL needed for ClearKey
+      },
+      clearKeys: {
+        '4146A8ECBB0540DC807C6389EE87E0BC': '0C3CDC1B3E4617C57361265E9FA4C5BD'
+      }
+    }
+  });
+
+  // Try to load a manifest.
+  // This is an asynchronous process.
+  try {
+    await player.load(manifestUri);
+    // This runs if the asynchronous load is successful.
+    console.log('The video has now been loaded!');
+  } catch (e) {
+    // onError is executed if the asynchronous load fails.
+    onError(e);
+  }
+}
+
+function onErrorEvent(event) {
+  // Extract the shaka.util.Error object from the event.
+  onError(event.detail);
+}
+
+function onError(error) {
+  // Log the error.
+  console.error('Error code', error.code, 'object', error);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
